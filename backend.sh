6baf266e73e7a321b723bf73c4b05dbce1fffb73
Ashwani <ashwaniankireddypalle@gmail.com>
2024-09-01 09:21:27 +0530

    adding mysql oot password

--- backend.sh
+++ backend.sh
@@ -27,7 +27,7 @@ VALIDATE(){
         echo -e "$R $2 is Failed....check it $N" | tee -a $LOG_FILE
         exit 1 
     else
-        echo -e "$G $2 is success .... completed $N" | tee -a $LOG_FILE
+        echo -e " $2 $G is success .... completed $N" | tee -a $LOG_FILE
     fi
 }
 
