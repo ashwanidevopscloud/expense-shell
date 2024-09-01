50d25802cf07e833b52ebb3523a35ba6ddaac2c1
Ashwani <ashwaniankireddypalle@gmail.com>
2024-09-01 07:36:43 +0530

    adding mysql oot password

---
+++ backend.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
+mkdir -p expense
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+uSERID=$(id -u)
+CHECK_ROOT(){
+    if [ $? -ne 0 ]
+    then
+       echo -e "$R I don't have root access, Please provide the root access with super pivellieges $N"
+    else
+       echo -e "$G I have root access.... I will procide with that....$N"
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2 is Failed....check it $N"
+    else
+        echo -e "$G $2 is success .... completed $N"
+    fi
+}
+
+echo -e "$Y Script started at execting at::: $(date) $N"
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "disable NODEJS"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "ENABLE NODEJS"
+
+dnf install nodejs -y
+VALIDATE $? "INSTALL NODEJS"
+
+useradd expense
+VALIDATE $? "ADDING useradd EXPENSE"
+
