f9769c4e0c0484e9c17e7464e395953f9877ab5a
Ashwani <ashwaniankireddypalle@gmail.com>
2024-09-01 09:16:08 +0530

    adding mysql oot password

--- backend.sh
+++ backend.sh
@@ -53,7 +53,7 @@ else
    echo -e "expense user already exists.... $Y Skipping it....$N" | tee -a $LOG_FILE
 fi
 
-mkdir -P /app  | tee -a $LOG_FILE
+mkdir --P /app  | tee -a $LOG_FILE
 VALIDATE $? "Ceating /app folder"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
